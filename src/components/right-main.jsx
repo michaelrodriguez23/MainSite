b2f35964956a38183e894bd0ac9fed192b0bd3e1
Michael Rodriguez <michaelrodriguez23@gmail.com>
2021-08-04 23:19:50 -0400

    hacked some resolution & media query fixes

--- src/components/right-main.jsx
+++ src/components/right-main.jsx
@@ -3,16 +3,20 @@ import ReactPlayer from "react-player";
 import { ProjectListContext } from "./ProjectListContext";
 import { InformationContext } from "./InformationContext";
 import Sketch from "./zoom-slider";
+import { useMediaPredicate } from "react-media-hook";
 import Styled from "styled-components";
+
 const RightMain = () => {
   const [projects, setProjects] = useContext(ProjectListContext);
   const [information, setInformation] = useContext(InformationContext);
+  const smallerThan400 = useMediaPredicate("(max-width: 400px)");
+  
   return (
     <>
       <DisplayData>
         {information.map((currentInfo, i) => (
           <>
-            <h1>{currentInfo.title}</h1>
+            <h3>{currentInfo.title}</h3>
             <h4>{currentInfo.date}</h4>
 
             <Brief>
@@ -25,11 +29,14 @@ const RightMain = () => {
             {currentInfo.image ? (
               <img src={process.env.PUBLIC_URL + currentInfo.image} />
             ) : null}
+            <ReactWrapper> 
+              
             {currentInfo.video ? (
-              <ReactPlayer  url={currentInfo.video} > </ReactPlayer>
+              <ReactPlayer height={smallerThan400 ? "200px" : "20em"} width={smallerThan400 ? "50em" : "100%"} className='react-player'  url={currentInfo.video} > </ReactPlayer>
               
             ) : null}
-            
+           
+            </ReactWrapper>
             <Credit>
               <a href={currentInfo.details}>{currentInfo.details} </a>
             </Credit>
@@ -48,22 +55,21 @@ export default RightMain;
 
 const DisplayData = Styled.div`
 text-align: center;
-
 background: black;
 font-size:1em;
-height:101vh;
+padding:1em;
 margin:0;
 color:white;
 @media (max-width: 768px) { 
-  text-align: center;
-
+text-align: center;
 background: black;
 font-size:.4em;
 padding:1em;
-height:20vh;
-width:90vw;
+height:15vh;
+width:100vw;
 margin:0;
 color:white;
+
  }
 `;
 
@@ -73,6 +79,8 @@ margin:0;
 font-size:1em;
 background-color:black;
 color:white;
+
+padding:1em;
 `;
 const Brief = Styled.p`
 text-align: center;
@@ -80,11 +88,25 @@ margin:0;
 font-size:1.3em ;
 background-color:black;
 color:white;
+
+
 `;
 const Credit = Styled.a`
+
 text-align: right;
 font-size:1em;
-
 background-color:black;
 color:white;
 `;
+// if webpage is larger than 1400px
+const ReactWrapper = Styled.div`
+@media (min-width: 1400px) { 
+padding-left:0em;
+padding:1em;
+}
+@media (min-width: 375) { 
+  padding-right:9em;
+  padding:1em;
+  }
+`;
+
